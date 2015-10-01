3a599d24991f5911a556f312f218a94204f3a12a
d34d <clark@cyngn.com>
2015-09-30 17:47:47 -0700

    Use correct DialtactsActivity for DialerNext
    DialerNext uses a different package name but the activity is still
    part of com.android.dialer
    
    Change-Id: Iafdffec268c1ecd7b2e5f818e85fb785a6e765ee

--- src/org/cyanogenmod/themes/provider/util/IconPreviewGenerator.java
+++ src/org/cyanogenmod/themes/provider/util/IconPreviewGenerator.java
@@ -25,7 +25,7 @@ public class IconPreviewGenerator {
     private static final ComponentName COMPONENT_DIALER =
             new ComponentName("com.android.dialer", "com.android.dialer.DialtactsActivity");
     private static final ComponentName COMPONENT_DIALERNEXT =
-            new ComponentName("com.cyngn.dialer", "com.cyngn.dialer.DialtactsActivity");
+            new ComponentName("com.cyngn.dialer", "com.android.dialer.DialtactsActivity");
     private static final ComponentName COMPONENT_MESSAGING =
             new ComponentName("com.android.mms", "com.android.mms.ui.ConversationList");
     private static final ComponentName COMPONENT_CAMERANEXT =
